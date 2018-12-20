9253e1315f3c13e44acb69085d39980c580c18c3
vagrant <vagrant@vagrant.vm>
2018-12-20 08:02:04 +0000

    edit another files

--- peer/register-enroll-peer.sh
+++ peer/register-enroll-peer.sh
@@ -28,7 +28,7 @@ fi
 
 # Function checks for the availability of the 
 function    checkCopyYAML {
-    SETUP_CONFIG_CLIENT_YAML="../setup/config/yaml.0/identities/$ORG_NAME/$PEER_NAME/fabric-ca-client-config.yaml"
+    SETUP_CONFIG_CLIENT_YAML="../setup/yaml.0/identities/$ORG_NAME/$PEER_NAME/fabric-ca-client-config.yaml"
     # Placeholder
     # This is not implemented - but is placed here to show how you can manage the CSR for the peers like other identities
 }
